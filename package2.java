bd208d2775227fda5ee85d627b7ba730b178d0c4
Palak Choudhary <123761109+palak2564@users.noreply.github.com>
2023-11-04 18:13:15 +0530

    Update package2.java

--- package2.java
+++ package2.java
@@ -1,6 +1,11 @@
 package usermanagement;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
+
 public class PortfolioManagementSystem {
     public static void main(String[] args) {
         System.out.println("Portfolio Management System");
@@ -10,17 +15,23 @@ public class PortfolioManagementSystem {
         System.out.print("Enter your choice: ");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
-
+        Connection con = null;
         switch (choice) {
             case 1:
-                login(con,scanner);
+                login(con, scanner);
                 break;
             case 2:
-                createAccount(con,scanner);
+                createAccount(con, scanner);
                 break;
             case 3:
                 System.out.println("Exiting the Portfolio Management System.");
-                con.close();
+                if (con != null) {
+                    try {
+                        con.close();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
                 System.exit(0);
                 break;
             default:
@@ -28,8 +39,7 @@ public class PortfolioManagementSystem {
         }
     }
 
-    private static void login() {
-        private static void login(Connection con, Scanner scanner) {
+    private static void login(Connection con, Scanner scanner) {
         try {
             System.out.print("Enter username: ");
             String usernameInput = scanner.next();
@@ -45,7 +55,7 @@ public class PortfolioManagementSystem {
             if (resultSet.next()) {
                 String username = resultSet.getString("username");
                 System.out.println("Login successful. Welcome, " + username + ".");
-                userMenu(con, scanner, username);
+
             } else {
                 System.out.println("Login failed. Please check your username and password.");
             }
@@ -54,33 +64,27 @@ public class PortfolioManagementSystem {
         }
     }
 
-    }
-
-    private static void createAccount() {
-        private static void login(Connection con, Scanner scanner) {
+    private static void createAccount(Connection con, Scanner scanner) {
         try {
             System.out.print("Enter username: ");
             String usernameInput = scanner.next();
             System.out.print("Enter password: ");
             String passwordInput = scanner.next();
 
-            String query = "SELECT username FROM users WHERE username = ? AND password = ?";
+            String query = "INSERT INTO users (username, password) VALUES (?, ?)";
             PreparedStatement pstmt = con.prepareStatement(query);
             pstmt.setString(1, usernameInput);
             pstmt.setString(2, passwordInput);
-            ResultSet resultSet = pstmt.executeQuery();
 
-            if (resultSet.next()) {
-                String username = resultSet.getString("username");
-                System.out.println("Login successful. Welcome, " + username + ".");
-                userMenu(con, scanner, username);
+            int rowsAffected = pstmt.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Account created successfully.");
             } else {
-                System.out.println("Login failed. Please check your username and password.");
+                System.out.println("Failed to create the account. Please try again.");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-
     }
 }
